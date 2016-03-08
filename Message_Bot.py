1804b63271b28e182958e4ff43aee2ee7b5377cc
mrtimdodge <mrtimdodge@gmail.com>
2016-03-08 15:16:45 -0600

    basic logic for submission scanning and subsequesnt comment scanning

--- Message_Bot.py
+++ Message_Bot.py
@@ -8,34 +8,73 @@ Change test
 """
 import praw
 import obot_jdmg as obot
+import time
+
 r = obot.login()
 author_name = "DapperDodger"
 subreddit_name = "test"
 start_word = "lumos"
 end_word = "nox"
+bot_word = "duelingbot!"
+poke_word = "poke"
 #keyword = "Hello World"
-keyword = "[Interactive] Dark SOULS"
-header = "Hello! " + author_name + " has made a new submission regarding " + keyword + " in /r/" +subreddit_name + " : \n\n"
+start_header = "Hello! A Trivia Game you wanted to know about has opened: \n\n"
+results_header = "Hello! Trivia Game results you wanted to know about are up: \n\n"
+
 footer = "\n\nI am a bot! If I'm acting up contact /u/" + author_name
 open_text = "#COMMENT HERE FOR A POKE WHEN QUIZ IS OPEN"
-def comment_parse():
-    for c in praw.helpers.comment_stream(r,subreddit_name, 1):
-        check_condition_start(c)     
-def send_messages(s):
+
+def comment_parse(s):
+    notStop = True
+    already_done = set()
+    while notStop:
+        s.refresh()
+        flat_comments = praw.helpers.flatten_tree(s.comments)
+        for comment in flat_comments:
+            if comment.id not in already_done:
+                notStop = check_condition_comments(comment)
+            already_done.add(comment.id)
+        time.sleep(20)
+            
+def send_start_messages(s):
     with open('users.txt') as f:
         users = f.readlines()
     for user in users:
-        r.send_message(user, s.title + " has started!", header + s.shortlink +footer)
+        r.send_message(user, s.title + " has started!", start_header + s.shortlink +footer)
+    open('users.txt', 'w').close()
+    
+def send_result_messages(s):
+    with open('users.txt') as f:
+        users = f.readlines()
+    for user in users:
+        r.send_message(user, s.title + " results!", results_header + s.shortlink +footer)
+    open('users.txt', 'w').close()
+    
 def add_comment(s,text):
     s.add_comment(text)
+    
 def check_condition_open(s):
     if s.link_flair_text == "Game":
             add_comment(s,open_text)
-            comment_parse()
-def check_condition_start(c):
-    if start_word.lower() in c.body:
-            send_messages(c.submission)
+            comment_parse(s)
+
+def check_condition_comments(c):
+    if start_word.lower() in c.body.lower() and bot_word.lower() in c.body.lower():
+            send_start_messages(c.submission)
+            return True
+    elif poke_word.lower() in c.body.lower():
+        if c.author:
+            f = open('users.txt','a')
+            f.write(c.author.name + "\n")
+            f.close()
+            return True
+    elif end_word.lower() in c.body.lower() and bot_word.lower() in c.body.lower():
+        send_result_messages(c.submission)
+        return False
+    else:
+        return True
             
         
 for s in praw.helpers.submission_stream(r,subreddit_name, 1):
     check_condition_open(s)
+
