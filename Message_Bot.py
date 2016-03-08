af7ba1bb2008393e7da4c71bca78b1fc588dd9f2
mrtimdodge <mrtimdodge@gmail.com>
2016-03-08 16:22:55 -0600

    ITS WORKING, ITS WORKING
    successful build of post the comment parsing

--- Message_Bot.py
+++ Message_Bot.py
@@ -18,8 +18,8 @@ end_word = "nox"
 bot_word = "duelingbot!"
 poke_word = "poke"
 #keyword = "Hello World"
-start_header = "Hello! A Trivia Game you wanted to know about has opened: \n\n"
-results_header = "Hello! Trivia Game results you wanted to know about are up: \n\n"
+start_header = "POKE! A Trivia Game you wanted to know about has opened: \n\n"
+results_header = "POKE! Trivia Game results you wanted to know about are up: \n\n"
 
 footer = "\n\nI am a bot! If I'm acting up contact /u/" + author_name
 open_text = "#COMMENT HERE FOR A POKE WHEN QUIZ IS OPEN"
@@ -27,60 +27,68 @@ open_text = "#COMMENT HERE FOR A POKE WHEN QUIZ IS OPEN"
 
 
 def comment_parse(s):
+    print "comment_parse"
     notStop = True
     already_done = set()
     while notStop:
         s.refresh()
         flat_comments = praw.helpers.flatten_tree(s.comments)
         for comment in flat_comments:
-            if comment.id not in already_done:
+            if comment.id not in already_done and comment.author and comment.author.name != "DuelingBot":
                 notStop = check_condition_comments(comment)
             already_done.add(comment.id)
         time.sleep(20)
             
 def send_start_messages(s):
+    print "send_start_messages"
     with open('users.txt') as f:
         users = f.readlines()
     for user in users:
-        r.send_message(user, s.title + " has started!", start_header + s.shortlink +footer)
+        r.send_message(user, s.title + " has started!", start_header + s.permalink +footer)
     open('users.txt', 'w').close()
     
 def send_result_messages(s):
+    print "send_result_messages("
     with open('users.txt') as f:
         users = f.readlines()
     for user in users:
-        r.send_message(user, s.title + " results!", results_header + s.shortlink +footer)
+        r.send_message(user, s.title + " results!", results_header + s.permalink +footer)
     open('users.txt', 'w').close()
     
 def add_comment(s,text):
+    print "add_comment"
     s.add_comment(text)
     
 def check_condition_open(s):
+    print "check_condition_open"
     if s.title == "Game":    
     #if s.link_flair_text == "Game":
             add_comment(s,open_text)
             comment_parse(s)
 
 def check_condition_comments(c):
+    print "check_condition_comments"
     authorized = []
     with open('authorized.txt') as fin:
         authorized = fin.readlines()
-    if start_word.lower() in c.body.lower() and bot_word.lower() in c.body.lower() and c.author in authorized:
+    if start_word.lower() in c.body.lower() and bot_word.lower() in c.body.lower() and c.author.name in authorized:
             send_start_messages(c.submission)
             return True
     elif poke_word.lower() in c.body.lower():
-        if c.author:
             f = open('users.txt','a')
             f.write(c.author.name + "\n")
             f.close()
+            print "added user: " + c.author.name 
             return True
-    elif end_word.lower() in c.body.lower() and bot_word.lower() in c.body.lower() and c.author in authorized:
+            
+    elif end_word.lower() in c.body.lower() and bot_word.lower() in c.body.lower() and c.author.name in authorized:
         send_result_messages(c.submission)
         return False
     else:
         return True
     
 if __name__=="__main__":
+   open('users.txt', 'w').close()
    for s in praw.helpers.submission_stream(r,subreddit_name, 1):
         check_condition_open(s)
         
