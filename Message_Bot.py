519d82eb871c9e09cd0c7ef5d936e07d7b28b7cb
mrtimdodge <mrtimdodge@gmail.com>
2016-03-09 10:59:02 -0600

    Changes to program flow, ability to give pecific results comment, README updated for this branch

--- Message_Bot.py
+++ Message_Bot.py
@@ -4,43 +4,48 @@ Created on Tue Mar  8 08:34:48 2016
 
 @author: DapperDodger
 
-Change test
 """
 import praw
 import obot_duel as obot
 import time
 
 r = obot.login()
-author_name = "DapperDodger"
-subreddit_name = "mousehuntcss"
-start_word = "lumos"
-end_word = "nox"
-bot_word = "duelingbot!"
-poke_word = "poke"
-#keyword = "Hello World"
-start_header = "POKE! A Trivia Game you wanted to know about has opened: \n\n"
-results_header = "POKE! Trivia Game results you wanted to know about are up: \n\n"
 
-footer = "\n\nI am a bot! If I'm acting up contact /u/" + author_name
-open_text = "#COMMENT HERE FOR A POKE WHEN QUIZ IS OPEN"
+author_name = "DapperDodger"
+subreddit_name = "mousehuntcss" #subreddit name
+start_word = "lumos" #send started messages keyword
+end_word = "nox" #send results messages keyword
+bot_word = "duelingbot!" #keyword to call bot
+poke_word = "poke" #keyword to be added to list
+message_word = "results" #word that has to be the message header to post results
+#keyword = "Hello World"
+start_header = "POKE! A Trivia Game you wanted to know about has opened: \n\n" #This is the header for the starting message
+results_header = "POKE! Trivia Game results you wanted to know about are up: \n\n" #This is the header for the results message
+footer = "\n\nI am a bot! If I'm acting up contact /u/" + author_name #General footer
+
+open_text = "#COMMENT HERE WITH KEYWORD '"+ poke_word + "' FOR A POKE WHEN QUIZ IS OPEN" #initial comment
+results_text = "#COMMENT HERE WITH KEYWORD "+ poke_word + "  FOR A POKE WHEN FULL RESULTS ARE UP" #results comment
+
+wait_time = 60 #time to wait between checking comments/messages in seconds
 
 
 
 def comment_parse(s):
-    print "comment_parse"
-    notStop = True
+    print "Starting to read comments for keyword"
+    finalResults = False
     already_done = set()
-    while notStop:
+    while not finalResults:
+        print "Reading comments..."
         s.refresh()
         flat_comments = praw.helpers.flatten_tree(s.comments)
         for comment in flat_comments:
             if comment.id not in already_done and comment.author and comment.author.name != "DuelingBot":
-                notStop = check_condition_comments(comment)
+                finalResults = check_condition_comments(comment)
             already_done.add(comment.id)
-        time.sleep(20)
+        time.sleep(wait_time)
             
 def send_start_messages(s):
-    print "send_start_messages"
+    print "Sending game started messages"
     with open('users.txt') as f:
         users = f.readlines()
     for user in users:
@@ -48,48 +53,69 @@ def send_start_messages(s):
     open('users.txt', 'w').close()
     
 def send_result_messages(s):
-    print "send_result_messages("
+    print "Sending results are up messages"
     with open('users.txt') as f:
         users = f.readlines()
     for user in users:
-        r.send_message(user, s.title + " results!", results_header + s.permalink +footer)
+        r.send_message(user, s.title + " results!", results_header + "/r/" + subreddit_name +footer)
     open('users.txt', 'w').close()
     
 def add_comment(s,text):
-    print "add_comment"
+    print "Adding comment to post"
     s.add_comment(text)
     
 def check_condition_open(s):
-    print "check_condition_open"
-    if s.title == "Game":    
-    #if s.link_flair_text == "Game":
+    print "Reading submissions..."
+    #if s.title == "Game":    
+    if s.link_flair_text and s.link_flair_text == "Game":
             add_comment(s,open_text)
             comment_parse(s)
 
 def check_condition_comments(c):
-    print "check_condition_comments"
+    print "Checking comment for keywords"
     authorized = []
     with open('authorized.txt') as fin:
         authorized = fin.readlines()
     if start_word.lower() in c.body.lower() and bot_word.lower() in c.body.lower() and c.author.name in authorized:
             send_start_messages(c.submission)
-            return True
+            wait_for_message()
+            return False
     elif poke_word.lower() in c.body.lower():
             f = open('users.txt','a')
             f.write(c.author.name + "\n")
             f.close()
-            print "added user: " + c.author.name 
-            return True
+            print "Added User: " + c.author.name 
+            return False
             
     elif end_word.lower() in c.body.lower() and bot_word.lower() in c.body.lower() and c.author.name in authorized:
         send_result_messages(c.submission)
-        return False
-    else:
         return True
-    
+    else:
+        return False
+        
+def wait_for_message():
+    print "Starting to wait for results message"
+    message_received = False
+    with open('authorized.txt') as fin:
+        authorized = fin.readlines()
+    while not message_received:
+        print "Waiting for results message..."
+        mail = r.get_unread()
+        if mail:
+            for message in mail:
+                if message.subject.lower() == message_word.lower() and message.author and message.author.name in authorized:
+                    add_comment(s, message.body + "\n\n" + results_text)
+                    message_received = True
+                message.mark_as_read()
+        time.sleep(wait_time)
 if __name__=="__main__":
    open('users.txt', 'w').close()
-   for s in praw.helpers.submission_stream(r,subreddit_name, 1):
-        check_condition_open(s)
+   print "Starting to check submission flair for keyword"
+           
+   try:
+       for s in praw.helpers.submission_stream(r,subreddit_name, 1):
+           check_condition_open(s)
+   except:
+       print "ERROR PLEASE RESTART BOT"
         
 
--- README.md
+++ README.md
@@ -1,10 +1,31 @@
-# Message-on-Submit-Bot
-You'll need to edit obot_jdmg to contain the correct OAuth info.
+# Message-on-Submit-Bot for /r/dueling
 
-Edit the Message_Bot.py file to change the author name, keyword, and subreddit name its looking for. 
+Before running edit authorized.txt to contain the names of users you want to be able to call the bot, one per line, no extra spaces
 
-Edit user.txt to contain the username of the users you want to send to, one per line, no extra spaces.
+Program flow:
 
-Run Message_Bot.py and make your submisison and it should send to the users listed in users.txt
+1. You start the bot, it will start reading the flair for submissions from /r/dueling, when you first start it it will read back one submission, so make sure the previous submission is NOT a flair with "Game" (only when you start it though)
 
+2. You (or anyone really) make a post with the flair "Game". It has to be that exact flair to work. 
 
+3. The bot posts a comment "#COMMENT HERE WITH KEYWORD poke FOR A POKE WHEN QUIZ IS OPEN"
+
+4. The bot starts reading comments for the keyword, if a comment has the keyword, it adds the user's name to a file
+
+5. You or any user authorized (I have a file named authorized.txt for that list) comments "Duelingbot! Lumos"
+
+6. The bot send out messages saying the game has started and gives a link back to the original post
+
+7. Bot sits and waits for a message with the word "Results" as its subject
+
+8. You (or anyone authorized) send a message with the word "Results" to the bot
+
+9. The bot posts a comment in the original post with the results and "#COMMENT HERE WITH KEYWORD poke FOR A POKE WHEN FULL RESULTS ARE UP"
+
+10. Bot goes back to reading comments for keyword
+
+11. You or You or any user authorized comments "Duelingbot! Nox"
+
+12. Bot sends out messages saying the results are up, and links back to the original post (not sure if thats what you wanted for this part?)
+
+13. Bot goes back to reading submissions and then back to step 2.
